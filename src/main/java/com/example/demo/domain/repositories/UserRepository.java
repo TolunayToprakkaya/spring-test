a48a6aab608a2d3ed4f2ba2c3f7d30747bcfd882
TolunayToprakkaya <tolunaytoprakkaya@gmail.com>
2018-03-18 19:16:00 +0300

    yeni bir endpoint eklendi

--- src/main/java/com/example/demo/domain/repositories/UserRepository.java
+++ src/main/java/com/example/demo/domain/repositories/UserRepository.java
@@ -3,7 +3,11 @@ package com.example.demo.domain.repositories;
 import com.example.demo.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RepositoryRestResource(path = "users", collectionResourceRel = "users")
 public interface UserRepository extends JpaRepository<User, String> {
+    @RestResource(path="findByName")
+    User findByName(@RequestParam("name")String name);
 }
